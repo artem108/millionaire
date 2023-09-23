c9937404bf439de186125fc0aecc40b4ac17a442
artem108 <artem.arshaniy108@gmail.com>
2023-09-23 23:28:18 +0300

    try to test base name for deploy

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -17,6 +17,6 @@ const router = createBrowserRouter([
     path: '/gameover/:totalreward',
     element: <GameOver />,
   },
-], { basename: 'millionaire/' });
+], { basename: process.env.PUBLIC_URL });
 
 export default router;
