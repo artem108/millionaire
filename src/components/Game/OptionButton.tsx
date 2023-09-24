9b144180e20c265ed9efd9d9c6d4b03f17c49065
artem108 <artem.arshaniy108@gmail.com>
2023-09-24 14:49:44 +0300

    test

--- src/components/Game/OptionButton.tsx
+++ src/components/Game/OptionButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { OptionButtonProps } from '../../interfaces';
 
-const OptionButton = ({ option, chooseAnswerHandler }: OptionButtonProps) => (
+const OptionButton = ({ option, chooseAnswerHandler}: OptionButtonProps) => (
   <div className="optionButton greyBorder">
     <span className="orangeOptionText">A</span>
     <button
